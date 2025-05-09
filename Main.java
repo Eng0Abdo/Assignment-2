b80401a5fcb9acfc0a815088529cff10ff64d350
MahmoudMohamed2004 <161710567+MahmoudMohamed2004@users.noreply.github.com>
2025-05-09 20:54:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Database db = new Database("accounts.dat");
-        Screen screen = new Screen(db);
+        Database db = new Database("accounts.db");
+        BankAccountDatabase bankDatabase = new BankAccountDatabase("bank.db");
+
+        Screen screen = new Screen(db, bankDatabase);
         Application app = new Application(screen);
         app.run();
     }
